b95c3f7bfab2676c534b9a91e1876330e51e18fb
Shafayet <shafayet.islam.61@gmail.com>
2020-07-31 01:41:23 +0600

    added more visualisation for analysing Questions

--- manipulationData.R
+++ manipulationData.R
@@ -13,6 +13,7 @@ df <- read.csv('spotifyTopListenedTrackAnalysi-final.csv')
 # Visualization starts
 View(df)
 library(ggplot2)
+library(dplyr)
 
 
 # As these are the songs I have listened more. What is most of the track's popularity 
@@ -39,7 +40,49 @@ ggplot(df, aes(x = key, fill = key))+
        y = "number of tracks",
        title = "Keys along with track counts")
 
-# Does loudness effects track Popularity? 
+# Does loud songs mean more energetic songs? most of the energy measured by loudness? 
+ggplot(df, aes(x = loudness, y = energy))+ 
+  theme_bw()+ 
+  geom_point()+
+  geom_smooth(method = "lm")+
+  labs(x = "loudness of track",
+         y = "energy of tracks",
+         title = "Effect of loudness in Measuring Energy")
+
+
+
+# interesting most popular track (what i listen) has either high Acoustic-ness or Low Acoustic-ness
+# nothing in the middle
+ggplot(df_new, aes(x = acousticness, y = track.Popularity, color = popularity_class, size = 0.4))+ 
+  theme_bw()+ 
+  geom_point()+
+  labs(x = "acousticness of track",
+       y = "Popularity of track",
+       title = "Acoustic-ness effects on popularity")
+
+
+# Does loudness and energy effects on track popularity?
+# creating A new column based on popularity
+# using dplyr
+
+df_new <- tbl_df(df) 
+dim(df_new)
+df_new <- mutate( df_new, popularity_class = ifelse( df_new$track.Popularity >= 1 & df_new$track.Popularity <= 20,"E",
+                                                     ifelse(df_new$track.Popularity > 20 & df_new$track.Popularity <=40, "D",
+                                                            ifelse(df_new$track.Popularity > 40 & df_new$track.Popularity <=60,"C",
+                                                                   ifelse(df_new$track.Popularity > 60 & df_new$track.Popularity <= 80, "B",
+                                                                          ifelse(df_new$track.Popularity > 80, "A","NULL" ) ) ) ) ) )
+
+
+
+
+
+
+ggplot(df_new, aes(x = popularity_class, fill = key ))+
+  theme_bw() +
+  geom_bar() +
+  labs( x ="Popularity Class (A(Highest)-E(Lowest))",
+        title = "")
 
 
 
